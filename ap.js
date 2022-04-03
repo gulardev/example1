ee434afca8502f7ffc4e6179a88533dec82c439b
Guler Guzel <gular@Gulers-MacBook-Pro.local>
2022-04-04 00:45:50 +0300

    completed V1

---
+++ ap.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("sideMenu").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("sideMenu").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+}
